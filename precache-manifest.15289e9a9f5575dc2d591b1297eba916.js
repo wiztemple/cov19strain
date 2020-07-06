2de187287523cb34577e5982027ffd84495a909d
Sullivan Wisdom <wiztemple7@gmail.com>
2020-07-06 13:35:22 +0100

    Updates

---
+++ precache-manifest.15289e9a9f5575dc2d591b1297eba916.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec10829ff12ef2bf9e0ef5816ae0da1",
+    "url": "/cov19strain/index.html"
+  },
+  {
+    "revision": "f294d012256f169a45b2",
+    "url": "/cov19strain/static/css/main.384bb0fe.chunk.css"
+  },
+  {
+    "revision": "4437c3b6eb9b2946f151",
+    "url": "/cov19strain/static/js/2.08766829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cov19strain/static/js/2.08766829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f294d012256f169a45b2",
+    "url": "/cov19strain/static/js/main.c2974f6f.chunk.js"
+  },
+  {
+    "revision": "e4bea16c727c542c3858",
+    "url": "/cov19strain/static/js/runtime-main.a8659fc4.js"
+  },
+  {
+    "revision": "2e6ebfe7abad1208653a05d1927683c2",
+    "url": "/cov19strain/static/media/coronavirus.2e6ebfe7.png"
+  },
+  {
+    "revision": "4cdd45efdeacab1609a7132fcb84c85c",
+    "url": "/cov19strain/static/media/grid.4cdd45ef.svg"
+  },
+  {
+    "revision": "8245e2630cf7123591ba3f6699b16f57",
+    "url": "/cov19strain/static/media/home.8245e263.svg"
+  }
+]);
